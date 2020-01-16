caa2fe2310764f1ad2f604792e9ed5992a1e026f
Dylan <technicalpro@securemedia.ch>
2020-01-16 22:42:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,13 +3,17 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-name = "Hi, my name is #{name}."
-introduction(name)
+def introduction(name)
+puts "Hi, my name is #{name}."
+#end
 
-language = "and I am learning to program in ruby"
-introduction_with_language(name,language)
+def introduction_with_language(name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
-introduction_with_language_optional =(name, language = Float)
+def introduction_with_language_optional(name, language="Ruby")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
 
 
